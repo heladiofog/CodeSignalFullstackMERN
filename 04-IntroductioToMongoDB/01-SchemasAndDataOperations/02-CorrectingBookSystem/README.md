1d5b9cb13cdb6109263b44ed5bda7b6d94241554
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-11-26 11:30:36 -0600

    Lesson2-Ex02-FindBooksByAuthor.

---
+++ 04-IntroductioToMongoDB/01-SchemasAndDataOperations/02-CorrectingBookSystem/README.md
@@ -0,0 +1,2 @@
+# Problem
+Your task is to adjust our book finder. Currently, it searches for a single book by its title. Change the function's purpose to finding all books by an author and rename it to `findBooksByAuthor`. Update the example usage accordingly.
---
+++ 04-IntroductioToMongoDB/01-SchemasAndDataOperations/02-CorrectingBookSystem/server/index.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const initialDBSetup = require('./initialDBSetup');
+const Book = require('./bookModel');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+mongoose
+  .connect('mongodb://127.0.0.1/myDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    console.log('connected');
+    // initialize DB, we will discuss this in further lessons
+    await initialDBSetup();
+  });
+
+// TODO: Update this function based on the instructions provided in the task description
+async function findBookByTitle(title) {
+  try {
+    return await Book.findOne({ title: title });
+  } catch (err) {
+    console.error('Error finding book by title:', err);
+  }
+}
+
+// Solution: findBookByAuthor function
+// TODO: Update this function based on the instructions provided in the task description
+async function findBookByAuthor(name) {
+  try {
+    return await Book.find({ author: name }); // Returns an array!!
+  } catch (err) {
+    console.error('Error finding book by author:', err);
+  }
+}
+
+app.get('/api/some-endpoint', async (req, res) => {
+  try {
+    // TODO: Don't forget to update the DB call too to take the author name 'Marijn Haverbeke'.
+    findBookByTitle('Eloquent JavaScript')
+      .then((book) => res.json({ message: JSON.stringify(book) }))
+      .catch((err) => console.error(err));
+    // Solution: call to findBookByAuthhor method:
+    // findBookByAuthor('Marijn Haverbeke')
+    //   .then((book) => res.json({ message: JSON.stringify(book) }))
+    //   .catch((err) => console.error(err));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
